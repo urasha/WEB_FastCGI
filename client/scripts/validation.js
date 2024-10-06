6598dbd978474ad65137ff67d568c21da9c81aa9
urasha <413022@niuitmo.ru>
2024-10-06 22:51:32 +0300

    radio buttons -> checkboxes

--- client/scripts/validation.js
+++ client/scripts/validation.js
@@ -24,7 +24,7 @@ async function validateInput(event) {
         return;
     }
 
-    const requestData = {
+    const request = {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
@@ -36,7 +36,7 @@ async function validateInput(event) {
         })
     };
 
-    fetch(MESSAGES.API_URL, requestData)
+    fetch(MESSAGES.API_URL, request)
         .then(response => response.json())
         .then(data => {
             addDataRow(xResult, yResult, rResult, data["isHit"], data["time"]);
@@ -68,8 +68,10 @@ function addDataRow(x, y, r, hit, time) {
 function getValidatedX() {
     const possibleXValues = [-4, -3, -2, -1, 0, 1, 2, 3, 4];
 
-    let xInput = document.querySelector('input[name="x-value"]:checked');
-    let x = xInput == null ? NaN : parseInt(xInput.value);
+    const xInput = document.querySelectorAll('input[name="x-value"]:checked');
+    let x = (xInput == null || xInput.length != 1) 
+            ? NaN 
+            : parseInt(xInput[0].value);
 
     let validationResult = !isNaN(x) && possibleXValues.includes(x);
     document.querySelector("#x-error").textContent = validationResult ? "" : MESSAGES.X_ERROR;
@@ -78,7 +80,7 @@ function getValidatedX() {
 }
 
 function getValidatedY() {
-    let yInput = document.querySelector('#y-value');
+    const yInput = document.querySelector('#y-value');
     let y = parseFloat(yInput.value);
 
     let validationResult = !isNaN(y) && y <= 5 && y >= -5;
@@ -90,7 +92,7 @@ function getValidatedY() {
 function getValidatedR() {
     const possibleRValues = [1, 1.5, 2, 2.5, 3];
 
-    let rInput = document.querySelector('#r-selection');
+    const rInput = document.querySelector('#r-selection');
     let r = parseInt(rInput.value);
 
     let validationResult = !isNaN(r) && possibleRValues.includes(r);
@@ -99,5 +101,5 @@ function getValidatedR() {
     return validationResult ? r : null;
 }
 
-let submitButton = document.querySelector("#submit-button");
+const submitButton = document.querySelector("#submit-button");
 submitButton.addEventListener("click", (event) => validateInput(event));
